17c525eee4b7ccfffe833f8655d9b67e7cf16d60
Jonathan León <jonathan-leon@hotmail.com>
2014-03-27 23:36:14 -0300

    añadidas 2 funciones
    añadida funcion  int DevolverMonto(char cadena[128])
    y + bool isInCharString(char cadena[128], string palabra)

--- main.cpp
+++ main.cpp
@@ -6,6 +6,45 @@
 
 using namespace std;
 
+//devuelve el monto de la transaccion de una fila en formato int
+int DevolverMonto(char cadena[128])
+{
+
+	string fila = cadena;
+	int num_comillas = 0;
+	string monto;
+	string monto_sin_comillas;
+	int monto_int;
+	for (int i = 0; i<fila.length(); i++)
+	{
+		if (fila[i] == '"')num_comillas++;
+		if (num_comillas == 4)
+		{
+			int pos = i + 1;
+			monto = fila.substr(pos);
+		}
+	}
+	int offset = 0;
+	for (int i = 0; i<monto.length() - 2; i++)offset++;
+	monto_sin_comillas = monto.substr(1, offset);
+	monto_int = atoi(monto_sin_comillas.c_str());
+	return monto_int;
+}
+
+//busca palabra en cadena
+bool isInCharString(char cadena[128], string palabra)
+{
+	const char *search = palabra.c_str();
+	for (int i = 0; i < strlen(cadena); ++i)
+	{
+		if (strncmp(&cadena[i], search, strlen(search)) == 0)
+			return true;
+	}
+
+	return false;
+}
+
+
 int main(int argc, char **argv)
 {
 
@@ -38,7 +77,7 @@ int main(int argc, char **argv)
 		cout << "Integrantes del grupo de trabajo : " << endl;
 		cout << "1.Jonathan Leon S , rut:17.771.116-0" << endl;
 		cout << "2.Francisca Sapiains Carrasco , rut:17707063-7" << endl;
-		cout << "3.Cristian Mondaca Ruiz , rut:15610050-1" << endl;		
+		cout << "3.Cristian Mondaca Ruiz , rut:15610050-1" << endl;
 		cout << "Fecha de compilacion: ";
 		puts(__DATE__);  //Fecha de compilacion.
 
@@ -50,7 +89,7 @@ int main(int argc, char **argv)
 	{
 		cout << "Error: Se necesitan parametros." << endl;
 	}
-	 
+
 
 
 }
